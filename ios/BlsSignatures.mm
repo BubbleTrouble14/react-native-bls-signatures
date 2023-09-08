bf0098a0159de9aee41e142a16e79d3272e23908
BubbleTrouble14 <ronald.goedeke@outlook.com>
2023-09-08 11:45:48 +0200

    lastest

--- ios/BlsSignatures.mm
+++ ios/BlsSignatures.mm
@@ -6,7 +6,9 @@
 #import "../cpp/TypedArray.h"
 // #import "AugSchemeMPLHostObject.h"
 #import "BlsSignatures.h"
-#import "bls.h"
+#import "blst.h"
+#import "sodium.h"
+#import "../cpp/include/bls/bls.hpp"
 
 using namespace bls;
 using namespace facebook;
@@ -39,11 +41,6 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(install) {
   }
   auto& runtime = *jsiRuntime;
 
-  vector<uint8_t> seed = {0,  50, 6,  244, 24,  199, 1,  25,  52,  88,  192,
-                        19, 18, 12, 89,  6,   220, 18, 102, 58,  209, 82,
-                        12, 62, 89, 110, 182, 9,   44, 20,  254, 22};
-
-  PrivateKey sk = AugSchemeMPL().KeyGen(seed);
 
   //AugSchemeMPL
   // auto AugSchemeMPL = jsi::Function::createFromHostFunction(
