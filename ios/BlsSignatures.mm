48e3275e5d60a8362bc7d06ae0d0a907fedeb6ff
Ronald Goedeke <ronald.goedeke@outlook.com>
2023-09-08 11:46:08 +0200

    test

--- ios/BlsSignatures.mm
+++ ios/BlsSignatures.mm
@@ -41,7 +41,11 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(install) {
   }
   auto& runtime = *jsiRuntime;
 
+  vector<uint8_t> seed = {0,  50, 6,  244, 24,  199, 1,  25,  52,  88,  192,
+                          19, 18, 12, 89,  6,   220, 18, 102, 58,  209, 82,
+                          12, 62, 89, 110, 182, 9,   44, 20,  254, 22};
 
+  PrivateKey sk = AugSchemeMPL().KeyGen(seed);
   //AugSchemeMPL
   // auto AugSchemeMPL = jsi::Function::createFromHostFunction(
   //                                                           runtime, jsi::PropNameID::forAscii(runtime, "AugSchemeMPL"), 0,
